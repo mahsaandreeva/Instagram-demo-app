13eb6da776ada0d9264eac9e14b8e333c48f2e5c
Maria Andreeva <andreeva.m4shenka@yandex.ru>
2020-10-24 09:32:16 +0300

    post

---
+++ src/post.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './post.css';
+
+export default function Post() {
+    return (
+        <div className="post">
+            <div>
+                <img className="postImg" src="https://sun9-26.userapi.com/impf/c854320/v854320585/18e145/m72bUXPpXpE.jpg?size=1280x962&quality=90&sign=7008879cf7b76d5b1cdbfb858e224778" />
+            </div>
+        </div>
+    )
+}
